5104b23d71cfb84aea76f995b145bb779fff000c
sctmic015 <michaelscott.scott77@gmail.com>
2022-08-03 11:10:15 +0200

    Neat Experimenting

---
+++ hexapod/controllers/anglequee.py
@@ -0,0 +1,7 @@
+class anglequee:
+    def __init__(self):
+        self.count = 0
+        self.first = []
+        self.second = []
+        self.third = []
+        sel.fourth = []
--- hexapod/controllers/testingNeat.py
+++ hexapod/controllers/testingNeat.py
@@ -32,6 +32,7 @@ class Controller:
 
     def __init__(self, params=tripod_gait, crab_angle=0.0, body_height=0.14, period=1.0, velocity=0.1, dt=1 / 240, ann = None, printangles = False, activations = 2):
         # link lengths
+        self.count = 0
         self.activations = activations
         self.ann = ann
         self.l_1 = 0.05317
@@ -77,16 +78,17 @@ class Controller:
         self.current_angle = initial_angle
 
     def joint_angles(self, t):
+
         sinewave = math.sin(t*2*math.pi)
         coswave = math.cos(t*2*math.pi)
         input_angles = np.append(self.current_angle, sinewave)
         input_angles = np.append(input_angles, coswave)
-        for i in range(self.activations):
-            current_angles = self.ann.activate(input_angles)
+        #for i in range(self.activations):
+        current_angles = self.ann.activate(input_angles)
         # Current theory is that the for loop makes the program to slow to do NEAT
         for i in range(len(current_angles)):
             if i % 3 == 0:
-                current_angles[i] = (current_angles[i] * math.pi) - (math.pi / 2)
+                current_angles[i] = (current_angles[i] * 2) - (2 / 2)
             elif i % 3 == 1:
                 current_angles[i] = current_angles[i] * 0.63
             else:
